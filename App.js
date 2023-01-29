1c03ea18ae38dcfb3833fae4ccb05ef5b8bdaaac
Abdul Aziz <95928540+Aziz7477@users.noreply.github.com>
2023-01-29 22:23:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
-
+let contents = document.querySelectorAll(".content");
+let listItems = document.querySelectorAll(".menu ul li");
+let hideAllContents = ()=>{
+    contents.forEach((content) => content.classList.remove("show"));
+}
+let hideAllItems = ()=>{
+    listItems.forEach((item) => item.classList.remove("active"));
+}
+listItems.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        hideAllContents();
+        hideAllItems();
+        item.classList.add("active");
+        contents[index].classList.add("show");
+    })
+})
